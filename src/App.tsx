a95cedaddbf0b1723d78e1c04ddac8f448889dd1
Houssam Jelliti <jelitihoussam@gmail.com>
2023-03-14 04:24:08 +0100

    feat: use FileUpload in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import FileUpload from "./FileUpload/FileUpload";
+
 function App() {
-  return <p>TDD with React.js</p>;
+  return (
+    <section>
+      <h1>TDD in React.js</h1>
+      <p>
+        In this example, we defined some stories and implemented them by
+        building a dummy component following a fully TDD workflow. This is not a
+        real app.
+      </p>
+      <p>For more, read: https://blog-jelhouss.vercel.app/posts/react-tdd</p>
+      <FileUpload />
+    </section>
+  );
 }
 
 export default App;
